8c465280442aeaca83f0eb58f13003e07525e5fa
Logan Wohlers <lawohlers@gmail.com>
2019-02-04 16:46:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves (a)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (a)
+  b=a.map do |s|
+    
 end
 
 def long_planeteer_calls# code an argument here
